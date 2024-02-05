6be79e1668ccf6e99bb871fad682be638078e2a9
evanqhu <evanqhu@tencent.com>
2024-02-05 16:00:39 +0800

    Quick commit - 2024-02-05 16:00:39

---
+++ quick_commit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 进入本地仓库目录
+cd /Users/hu/胡强/GitHub/vue-admin-master
+
+# 检查是否存在未提交的修改
+if [ -n "$(git status --porcelain)" ]; then
+  # 存在未提交的修改
+  # 添加所有修改
+  git add .
+
+  # 提交修改
+  git commit -m "Quick commit - $(date '+%Y-%m-%d %H:%M:%S')"
+
+  # 推送到远程仓库
+  git push origin master # 或者使用你当前分支的名称
+else
+  echo "没有未提交的修改."
+fi
